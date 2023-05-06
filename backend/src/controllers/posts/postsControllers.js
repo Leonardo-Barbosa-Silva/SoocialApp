f28430e6465ed946f18e78c3a5e43846b03082aa
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-05-05 23:37:20 -0300

    posts controllers

--- backend/src/controllers/posts/postsControllers.js
+++ backend/src/controllers/posts/postsControllers.js
@@ -7,6 +7,142 @@ export default {
     // @route POST v1/api/posts/create
     // @access Private
     createPost: async (req, res) => {
+        try {
 
-    }
+            const {
+                description,
+                picturePath
+            } = req.body
+
+            if (!req.user._id) {
+                res.status(404).json({ message: "User not logged. Please, login in again." })
+                return
+            }
+
+            const postCreated = await postsModel.create({
+                userID: req.user._id,
+                firstName: req.user.firstName,
+                lastName: req.user.lastName,
+                location: req.user.location,
+                description,
+                picturePath,
+                userPicturePath: req.user.picturePath,
+                likes: {},
+                comments: []
+            })
+
+            res.status(201).json({
+                message: "Successfully created a new post.",
+                item: postCreated
+            })
+
+        } catch (error) {
+            console.log(`${error}`.red)
+
+            if (error instanceof mongoose.Error.ValidationError) {
+                return res.status(400).json({ message: error.message })
+            } else if (error instanceof mongoose.Error.CastError) {
+                return res.status(400).json({ message: error.message })
+            } else {
+                return res.status(409).json({
+                    error: "Internal server error. Please, try again later."
+                })
+            }
+        }
+    },
+
+    // @desc Get all posts in db
+    // @route GET v1/api/posts/all
+    // @access Private
+    getFeedPosts: async (req, res) => {
+        try {
+
+            if (!req.user._id) {
+                res.status(404).json({ message: "User not logged. Please, login in again." })
+                return
+            }
+
+            const posts = await postsModel.find()
+
+            res.status(201).json({
+                message: "Successfully created a new post.",
+                item: posts
+            })
+
+        } catch (error) {
+            console.log(`${error}`.red)
+        }
+    },
+
+    // @desc Get the user posts
+    // @route GET v1/api/posts/user/:userID
+    // @access Private
+    getUserPosts: async (req, res) => {
+        try {
+
+            const { userID } = req.params
+
+            if (!req.user._id) {
+                res.status(404).json({ message: "User not logged. Please, login in again." })
+                return
+            }
+
+            const userPosts = await postsModel.find({ userID })
+
+            res.status(201).json({
+                message: "Successfully created a new post.",
+                item: userPosts
+            })
+
+        } catch (error) {
+            console.log(`${error}`.red)
+            return (
+                res.status(409).json({ error: "Internal server error. Please, try again later." })
+            )
+        }
+    },
+
+    // @desc Add or remove user like on posts
+    // @route PATCH v1/api/posts/:postID/likes
+    // @access Private
+    likePosts: async (req, res) => {
+        try {
+
+            const { postID } = req.params
+
+            if (!req.user._id) {
+                res.status(404).json({ message: "User not logged. Please, login in again." })
+                return
+            }
+
+            // GET THE POST
+            const post = await postsModel.findById(postID)
+
+            // VERIFY IF THE USER ALREADY LIKED THE POST
+            const likeExists = post.likes.get(req.user._id)
+
+            // REMOVE USER LIKE OR ADD IT
+            if (likeExists) {
+                post.likes.delete(req.user._id)
+            } else {
+                post.likes.set(req.user._id, true)
+            }
+
+            const postUpdated = postsModel.findByIdAndUpdate(
+                postID,
+                { likes: post.likes },
+                { new: true }
+            )
+
+            res.status(200).json({
+                item: postUpdated
+            })
+
+        } catch (error) {
+            console.log(`${error}`.red)
+            return (
+                res.status(409).json({ error: "Internal server error. Please, try again later." })
+            )
+        }
+    },
 }
--- backend/src/routes/posts/postsRoutes.js
+++ backend/src/routes/posts/postsRoutes.js
@@ -1,30 +1,30 @@
-
 import express from 'express';
-/*
 import authUserToken from '../../middlewares/auth.js';
-import { upload } from '../../server.js'
-import {
+import { upload } from '../../configs/multer.js';
+import postsControllers from '../../controllers/posts/postsControllers.js';
+
+
+// DESCTRUCTURING POSTS CONTROLLERS
+const {
     createPost,
     getFeedPosts,
     getUserPosts,
     likePosts
-} from '../../controllers/posts/postsControllers';
-*/
+} = postsControllers
 
 // EXPRESS ROUTER
 const router = express.Router()
-/*
+
 // CREATE POST
 router.post("/create", authUserToken, upload.single("picture"), createPost)
 
 // GET POSTS
-router.get("/me", authUserToken, getFeedPosts)
+router.get("/all", authUserToken, getFeedPosts)
 router.get("/:userID", authUserToken, getUserPosts)
 
 // UPDATE STATUS POSTS
-router.patch("/:id/likes", authUserToken, likePosts)
+router.patch("/:postID/likes", authUserToken, likePosts)
 
 
 
-*/
 export default router;
