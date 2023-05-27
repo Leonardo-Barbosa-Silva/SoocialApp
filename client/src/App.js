50c6889461828a6f5b2baf5c1b044e4acf49f268
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-05-27 09:16:34 -0300

    mui theme

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,33 @@ import HomePage from './pages/Home/index.jsx';
 import LoginPage from './pages/Login/index.jsx';
 import RegisterPage from './pages/Register/index.jsx';
 import ProfilePage from './pages/Profile/index.jsx';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme/index.js';
 
 
 function App() {
+
+  // GET MODE IN REDUX USERS STATE
+  const mode = useSelector( state => state.mode )
+
+  // CREATE MUI MODE THEME
+  const theme = useMemo( () => createTheme(themeSettings(mode)), [mode] )
+
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/profile/:userId' element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/profile/:userId' element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </Router>
     </div>
   );
